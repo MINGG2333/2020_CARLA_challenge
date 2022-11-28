f13259d17de271a0e26961731c5d69ddaf52730c
MINGG2333 <54434036+MINGG2333@users.noreply.github.com>
2022-11-28 20:03:50 +0800

    copy from MaRLn

---
+++ bird_view/models/config.py
@@ -0,0 +1,37 @@
+import os
+
+import torch
+
+class GlobalConfig:
+    """ base architecture configurations """
+
+    path_folder_model = '../2020_CARLA_challenge/model_RL_IAs_CARLA_Challenge/'
+
+    nb_action_steering = 27
+    max_steering = 0.6
+    nb_action_throttle = 3
+    max_throttle = 1.0
+    front_camera_width = 288
+    front_camera_height = 288
+    front_camera_fov = 100
+    crop_sky = False
+
+    # IQN parameters
+    kappa = 1.0
+    num_tau_samples = 8
+    num_tau_prime_samples = 8
+    num_quantile_samples = 32
+    quantile_embedding_dim = 64
+
+    steps_image = [
+        -10,
+        -2,
+        -1,
+        0,
+    ]
+
+    device = torch.device('cuda')
+
+    def __init__(self, **kwargs):
+        for k,v in kwargs.items():
+            setattr(self, k, v)
